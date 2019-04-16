79c665197be2239ff25e6a7a8cc594e3d9fb424f
JShull97 <38473222+JShull97@users.noreply.github.com>
2019-04-16 12:45:10 -0500

    add sandwich size prompt

--- GroupProjectSubway/src/groupprojectsubway/GroupProjectSubway.java
+++ GroupProjectSubway/src/groupprojectsubway/GroupProjectSubway.java
@@ -11,8 +11,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -30,15 +34,29 @@ public class GroupProjectSubway extends Application{
         buttons.getChildren().add(placeOrder);
         buttons.setAlignment(Pos.CENTER);
         
+        // Start sandwich size
+        RadioButton sixInch = new RadioButton("Six Inch");
+        RadioButton footLong = new RadioButton("Footlong");
+        ToggleGroup tg = new ToggleGroup();
+        sixInch.setToggleGroup(tg);
+        footLong.setToggleGroup(tg);
+        VBox sizeBox = new VBox(5);
+        sizeBox.getChildren().addAll(new Label("What size sandwich do you want?"), sixInch, footLong);
+        // end sandwich size
+        
         Button bread = new Button();
         bread.setText("italian");
-//        bread.getChildren().add(bread);
+        //bread.getChildren().add(bread);
 
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(20));
         root.setBottom(buttons);
         root.setLeft(bread);
+        root.setTop(sizeBox);
 
+        // display order event listener
+        placeOrder.setOnAction(e -> displayOrder());
+        
         Scene scene = new Scene(root, 600, 250);
 
         primaryStage.setTitle("Subway Order System");
@@ -49,6 +67,10 @@ public class GroupProjectSubway extends Application{
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public void displayOrder() {
+        // TODO
     }
 
 }
